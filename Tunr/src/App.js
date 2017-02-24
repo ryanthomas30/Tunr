2c8ccd5b40f8840678cf5a0a427a43aa97963065
Ryan Thomas <ryanthomas30@gmail.com>
2017-02-24 02:40:02 -0500

    Add Button

---
+++ Tunr/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Sound from 'react-sound';
+import './App.css';
+import Button from './Button.js';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			note : null
+		};
+	}
+
+	_ChangeSound(newNote) {
+		switch (newNote) {
+			case 'E2':
+				if(this.state.note === newNote) {
+					this.setState({note : null});
+				} else {
+					this.setState({note : 'E2'});
+				}
+				break;
+			case 'A2':
+				if(this.state.note === newNote) {
+					this.setState({note : null});
+				} else {
+					this.setState({note : 'A2'});
+				}
+				break;
+			default:
+				break;
+		}
+		console.log(this.state);
+	}
+
+	_PlayStatus = () => {
+		if(this.state.note === null){
+			return Sound.status.STOPPED;
+		} else {
+			return Sound.status.PLAYING;
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Sound
+					url="E2.mp3"
+					playStatus={this._PlayStatus} />
+				<div className="App-header">
+					<h1>Tunr</h1>
+				</div>
+				<div className="Buttons">
+					<Button
+						changeSound={this._ChangeSound}
+						newNote='E2'
+						noteTitle='E' />
+					<Button
+						changeSound={this._ChangeSound}
+						newNote='A3'
+						noteTitle='A' />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ Tunr/src/Button.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Sound from 'react-sound';
+import './Button.css';
+
+class Button extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return(
+			<button className='Button' onClick={() => this.props.changeSound(this.props.newNote)}>{this.props.noteTitle}</button>
+		)
+	}
+}
+
+export default Button;
